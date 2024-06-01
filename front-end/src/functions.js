fe5de9127d7ec7078a2d54b365388cd194bbb055
Alexander Yordanov <alex05.yordanov@gmail.com>
2024-06-01 18:49:37 +0000

    I have no clue what I'm committing

--- front-end/src/functions.js
+++ front-end/src/functions.js
@@ -8,7 +8,7 @@ API_BASE_URL = "http://127.0.0.1:8000"
 
 try {
     if (window.location.hostname != '') {
-        API_BASE_URL = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
+        API_BASE_URL = `${window.location.protocol}//${window.location.hostname}/api`
     }
 } 
 catch {
