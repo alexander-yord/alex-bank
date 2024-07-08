a35fe8a5fc14521ee4f3af90b5e3e1a03a21d844
Alexander Yordanov <alex05.yordanov@gmal.com>
2024-07-09 00:02:21 +0300

    Added a function which verifies the token. And on each page wherever necessary, I added an invocation. Login redirect is now automatic

--- front-end/src/functions.js
+++ front-end/src/functions.js
@@ -1,10 +1,26 @@
+API_BASE_URL = "http://127.0.0.1:8000/api"
+
 function isLoggedIn() {
     const requiredKeys = ['account_id', 'first_name', 'last_name', 'user_role', 'token'];
     
     return requiredKeys.every(key => sessionStorage.getItem(key) !== null);
 }
 
-API_BASE_URL = "http://127.0.0.1:8000/api"
+async function redirectIfNotLoggedIn() {
+    const response = await fetch(`${API_BASE_URL}/auth/verify`, {
+        method: "GET", 
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${sessionStorage.getItem("token")}`
+        }
+    });
+    if (response.status == 401) {
+        sessionStorage.setItem("redirectURL", window.location.href);
+        window.location.href = "/login/index.html";
+        return;
+    }
+    return ;
+}
 
 try {
     if (window.location.hostname != '' && window.location.hostname != '127.0.0.1') {
