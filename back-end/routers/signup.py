b8bf766cbe97c8347cebd2da6cd21b8160a97671
Alexander Yordanov <alex05.yordanov@gmal.com>
2024-09-11 10:32:07 -0400

    DEV-17 bugfix on POST /signup/ endpoint, as cnx.commit() was still referrencing the old db.cnx.commit()

--- back-end/routers/signup.py
+++ back-end/routers/signup.py
@@ -20,7 +20,7 @@ async def signup(acc_info: s.NewAccount = None):
                "verification, account_group) VALUES (%s, %s, %s, %s, %s, %s, 'U', 'N', 'FTC')"
         cursor.execute(stmt, (acc_info.first_name, acc_info.last_name, acc_info.email, acc_info.phone,
                               acc_info.country, acc_info.address))
-        db.cnx.commit()
+        cnx.commit()
 
         stmt2 = "SELECT account_id FROM accounts WHERE first_name = %s AND last_name = %s ORDER BY 1 DESC"
         cursor.execute(stmt2, (acc_info.first_name, acc_info.last_name))
@@ -34,7 +34,7 @@ async def signup(acc_info: s.NewAccount = None):
 
         stmt3 = "INSERT INTO login_credentials (account_id, password) VALUES (%s, %s)"
         cursor.execute(stmt3, (account_id, hashed_password))
-        db.cnx.commit()
+        cnx.commit()
 
         token = h.create_jwt_token(account_id, 'U')
 
